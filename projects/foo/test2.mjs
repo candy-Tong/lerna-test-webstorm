0af82039d6142ebe1802856a23c43ca17b80f3e0
candychuang <candychuang@tencent.com>
2022-07-05 14:31:57 +0800

    update

--- projects/foo/test2.mjs
+++ projects/foo/test2.mjs
@@ -1,2 +1,2 @@
-import test1 from '@amlmp/foo/src/test1.mjs'
+import {test1} from '@amlmp/foo/src/test1.mjs'
 console.log(test1)
--- projects/test/index.mjs
+++ projects/test/index.mjs
@@ -1,2 +1,4 @@
 import {foo} from '@amlmp/foo/index.mjs'
+import vue from '@amlmp/foo/src/index.vue'
 console.log(foo)
+console.log(vue)
